f49005c07cb293de45182124c510258d124a854e
Chetan Pandey <163017526+chetan-spheron@users.noreply.github.com>
2025-03-17 19:34:49 +0530

    binary url updated (#12)

--- scripts/k3s-bootstrap.sh
+++ scripts/k3s-bootstrap.sh
@@ -326,7 +326,7 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 #Install Spheron and setup wallet
 function install_spheron(){
-wget  https://spheron-release.s3.amazonaws.com/bins/amd64/spheron
+wget  https://release.sphnctl.sh/bins/amd64/spheron
 cp spheron /usr/local/bin/sphnctl
 chmod +x /usr/local/bin/sphnctl
 echo "Spheorn Version     : $(sphnctl version)"
